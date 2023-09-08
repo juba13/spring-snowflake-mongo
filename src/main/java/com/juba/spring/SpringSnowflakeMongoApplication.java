d504a4bb3ec7a77c054677e0edaca31a8f01a1f9
Mohammed Jubayer <mohammed.jubayer@gmail.com>
2023-09-08 16:26:53 +0600

    Init spring project

---
+++ src/main/java/com/juba/spring/SpringSnowflakeMongoApplication.java
@@ -0,0 +1,13 @@
+package com.juba.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSnowflakeMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSnowflakeMongoApplication.class, args);
+	}
+
+}
