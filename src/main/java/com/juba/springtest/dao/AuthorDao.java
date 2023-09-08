9acd8f276d475dbaffc295b193bf7487545cbbf1
Mohammed Jubayer <mohammed.jubayer@gmail.com>
2023-09-08 20:00:50 +0600

    Add auhor DAO interface

---
+++ src/main/java/com/juba/springtest/dao/AuthorDao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.juba.springtest.dao;
+
+import com.juba.springtest.model.Author;
+import java.util.List;
+
+/**
+ *
+ * @author mohammed
+ */
+public interface AuthorDao {
+      List<Author> getAll();
+      Author getById(String authorId);
+      Author save(Author author);
+}
