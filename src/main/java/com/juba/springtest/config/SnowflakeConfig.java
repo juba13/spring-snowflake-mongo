1ba62e89ead99ad695719be0185c414030c30f8b
Mohammed Jubayer <mohammed.jubayer@gmail.com>
2023-09-09 18:00:59 +0600

    Added snowflack custome connection

---
+++ src/main/java/com/juba/springtest/config/SnowflakeConfig.java
@@ -0,0 +1,54 @@
+package com.juba.springtest.config;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SnowflakeConfig {
+    
+    
+    @Value("${spring.datasource.username}")
+    private String username;
+    
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+    
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverName;
+    
+    
+    @Value("${spring.datasource.hikari.connection-timeout}")
+    private int timeout;
+    
+    
+    @Value("${spring.datasource.hikari.maximum-pool-size}")
+    private int poolSize;
+    
+    @Bean
+    public HikariDataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driverName);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setMaximumPoolSize(10);
+        config.setConnectionTimeout(6000);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+}
---
+++ src/main/java/com/juba/springtest/config/SnowflakeDialect.java
@@ -0,0 +1,5 @@
+package com.juba.springtest.config;
+import org.hibernate.dialect.Dialect;
+
+public class SnowflakeDialect extends Dialect {
+}
